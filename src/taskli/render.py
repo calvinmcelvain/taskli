f522b1f9f4a7fd411f11e79c185f434a48075a3b
calvin-mcelvain <mcelvain1@hotmail.com>
2026-08-17 07:25:04 -0400

    refactor: Renaming 'Done' --> 'State'

--- src/taskli/render.py
+++ src/taskli/render.py
@@ -54,7 +54,7 @@ def _items_table(items: list[TaskliItem], color: Color | None = None) -> Table:
 
     table = Table()
     table.add_column(_add_color("ID", color), justify="right")
-    table.add_column(_add_color("Done", color), justify="center")
+    table.add_column(_add_color("State", color), justify="center")
     table.add_column(_add_color("Text", color), justify="left")
     table.add_column(_add_color("Priority", color), justify="left")
     table.add_column(_add_color("Tags", color), justify="left")
